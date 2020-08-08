e6dfa16ac544f1e96806ccf4220eee3224447c14
marwa.brj@gmail.com <marwa.brj@gmail.com>
2020-08-08 14:32:50 +0200

    lab OK

--- app.js
+++ app.js
@@ -2,10 +2,20 @@ const express = require('express');
 
 const app = express();
 
-app.get('/home', (request, response, next) => {});
+app.use(express.static('public'));
+app.get('/home', (request, response, next) => {
+    response.sendFile(__dirname+'/views/home.html')
+});
 
-app.get('/about', (request, response, next) => {}); 
+app.get('/about', (request, response, next) => {
+    response.sendFile(__dirname+'/views/about.html')
+}); 
 
-app.get('/works', (request, response, next) => {}); 
+app.get('/works', (request, response, next) => {
+    response.sendFile(__dirname+'/views/works.html')
+}); 
 
-app.get('/gallery', (request, response, next) => {}); 
+app.get('/gallery', (request, response, next) => {
+    response.sendFile(__dirname+'/views/gallery.html')
+}); 
+app.listen(3000);
