02dec303c9b9ce79dc6c305b0d5dba2da2af45fa
Lekmiti Wafaa <lekmiti.wafaa@gmail.com>
2020-08-08 12:39:27 +0200

    server created +  4 pages created

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/home', (request, response, next) => {});
+
+app.get('/about', (request, response, next) => {}); 
+
+app.get('/works', (request, response, next) => {}); 
+
+app.get('/gallery', (request, response, next) => {}); 
